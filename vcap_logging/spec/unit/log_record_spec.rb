ae8699e6da243458a0409d719c71412fb2d2e1c2
mpage <mpage@vmware.com>
2011-05-24 19:53:46 -0700

    Topic: merge Flesh out log records and add a basic character delimited formatter.
    This adds the following fields to log records:
    
    - logger name
    - timestamp
    - thread_id / short id
    - fiber_id  / short id
    - process_id
    
    Change-Id: I1dea0359ee9ef9d951b84a411fc14b6f75a6cb60

--- vcap_logging/spec/unit/log_record_spec.rb
+++ vcap_logging/spec/unit/log_record_spec.rb
@@ -1,6 +1,51 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
+require 'thread'
+
 describe VCAP::Logging::LogRecord do
+  before :each do
+    @logger = VCAP::Logging::Logger.new('bar', nil)
+    @tags = ['zaz']
+    @data = 'foo'
+    @log_level = :debug
+    @rec = VCAP::Logging::LogRecord.new(@log_level, @data, @logger, @tags)
+  end
+
   describe '#initialize' do
+    it "sets the current thread id and short id" do
+      @rec.thread_id.should == Thread.current.object_id
+      @rec.thread_shortid.should_not == nil
+    end
+
+    it "sets the current process id" do
+      @rec.process_id.should == Process.pid
+    end
+
+    it "sets the current fiber id and short id" do
+      begin
+        require 'fiber'
+        @rec.fiber_id.should == Fiber.current.object_id
+        @rec.fiber_shortid.should_not == nil
+      rescue LoadError
+        @rec.fiber_id.should == nil
+        @rec.fiber_shortid.should == nil
+      end
+    end
+
+    it "sets the logger name" do
+      @rec.logger_name.should == @logger.name
+    end
+
+    it "sets the timestamp of when the record was created" do
+      @rec.timestamp.should_not be_nil
+    end
+
+    it "sets tags" do
+      @rec.tags.should == @tags
+    end
+
+    it "sets the log level" do
+      @rec.log_level.should == @log_level
+    end
   end
 end
