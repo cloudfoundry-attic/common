8e007d0ca3ac0ad40b39f48b90b314e4742a50f8
mpage <mpage@vmware.com>
2011-05-24 19:53:46 -0700

    Flesh out log records and add a basic character delimited formatter.
    This adds the following fields to log records:
    
    - logger name
    - timestamp
    - thread_id / short id
    - fiber_id  / short id
    - process_id
    
    Change-Id: I1dea0359ee9ef9d951b84a411fc14b6f75a6cb60

--- vcap_logging/lib/vcap/logging/log_record.rb
+++ vcap_logging/lib/vcap/logging/log_record.rb
@@ -1,17 +1,71 @@
+require 'digest/md5'
+require 'thread'
+
 module VCAP
   module Logging
 
     class LogRecord
+
+      @@have_fibers = nil
+
+      class << self
+
+        def have_fibers?
+          if @@have_fibers == nil
+            begin
+              require 'fiber'
+              @@have_fibers = true
+            rescue LoadError
+              @@have_fibers = false
+            end
+          else
+            @@have_fibers
+          end
+        end
+
+        def current_fiber_id
+          if have_fibers?
+            Fiber.current.object_id
+          else
+            nil
+          end
+        end
+
+      end
+
+      attr_reader :timestamp
       attr_reader :data
       attr_reader :log_level
+      attr_reader :logger_name
       attr_reader :tags
+      attr_reader :thread_id
+      attr_reader :thread_shortid
+      attr_reader :fiber_id
+      attr_reader :fiber_shortid
+      attr_reader :process_id
 
-      def initialize(log_level, data, tags=[])
-        @data      = data
-        @log_level = log_level
-        @tags      = tags
+      def initialize(log_level, data, logger, tags=[])
+        @timestamp   = Time.now
+        @data        = data
+        @logger_name = logger.name
+        @log_level   = log_level
+        @tags        = tags
+
+        @thread_id      = Thread.current.object_id
+        @thread_shortid = shortid(@thread_id)
+        @fiber_id       = LogRecord.current_fiber_id
+        @fiber_shortid  = @fiber_id ? shortid(@fiber_id) : nil
+        @process_id     = Process.pid
       end
-    end
 
+      private
+
+      def shortid(data, len=4)
+        digest = Digest::MD5.hexdigest(data.to_s)
+        len = len > digest.length ? digest.length : len
+        digest[0, len]
+      end
+
+    end # VCAP::Logging::LogRecord
   end
 end
